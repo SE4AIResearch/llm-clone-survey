12c085a64fb78bf7a9dd36cb487819d9f18ed1fd
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-07-02 21:24:59 -0400

    Processing duplicates

--- scripts/deduplicate.py
+++ scripts/deduplicate.py
@@ -1,36 +1,39 @@
-import pandas as pd
+import sqlite3
+
 from Levenshtein import distance
+
 THRESHOLD = 5
+if __name__ == "__main__":
+    # Connect to the SQLite database
+    conn = sqlite3.connect('../../coding_website/db_llm_education_survey.sqlite3')
 
-df = pd.read_csv('new_data.csv')
-df = df[['cited_by', 'Title']]
-df['Title'] = df['Title'].astype(str)
+    # Create a cursor object
+    cursor = conn.cursor()
 
-for paper in df['Title']:
-    firstChar = paper[0]
-    if firstChar.isdigit():
-        index = paper.find('.')
-        paper = paper[index+2:]
-    elif firstChar == '[':
-        index = paper.find(']')
-        paper = paper[index+2:]
+    # Execute a SQL query
+    cursor.execute('SELECT id, title, source FROM llm_education_survey_paper')
 
-df = df.drop_duplicates(subset=['Title'])
+    # Fetch all results from the executed query
+    rows = cursor.fetchall()
 
-with open("duplicates.txt", "w") as f:
+    # Compose paper list
+    papers = [r for r in rows]
 
-    for paper in range(len(df['Title'])):
-        for compare in range(len(df['Title'])):
-            if paper != compare:
-                    edit_distance = distance(df.iloc[paper,1], df.iloc[compare,1])
+    with (open("../results/possible_duplicates.tsv", "w") as f):
+        f.write(f"paper1_id\tpaper2_id\tsource1\tsource2\tcombined_sources\tpaper1\tpaper2\tedit_distance\n")
+        for paper1_id, paper1, source1 in papers:
+            for paper2_id, paper2,source2 in papers:
+                if paper1_id != paper2_id and \
+                    paper1_id < paper2_id and \
+                        len(paper1) > THRESHOLD and \
+                        len(paper2) > THRESHOLD and \
+                        source1 != source2:
+                    edit_distance = distance(paper1, paper2)
                     if edit_distance <= THRESHOLD:
-                        f.write("POSSIBLE DUPLICATES")
-                        og = str(paper) + " " + df.iloc[paper,1]
-                        compare = str(compare) + " " + df.iloc[compare,1]
-                        f.write(og)
-                        f.write(compare)
-                        f.write('\n')
-                        print('found')
+                        clean_paper1 = paper1.replace('\t', ' ').replace('\n', ' ')
+                        clean_paper2 = paper2.replace('\t', ' ').replace('\n', ' ')
+                        combined_sources = f"{source1}, {source2}"
+                        f.write(f"{paper1_id}\t{paper2_id}\t{source1}\t{source2}\t{combined_sources}\t{clean_paper1}\t{clean_paper2}\t{edit_distance}\n")
 
-
-
+    # Close the connection
+    conn.close()
